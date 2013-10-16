582e8242cc455b04f12db035775693c43941a6d3
Matt Newton <newton10471@gmail.com>
2013-10-16 07:59:15 -0400

    comment out nonworking code

--- namespace.js
+++ namespace.js
@@ -1,21 +1,19 @@
 function namespace(args) {
   args = args.split('.');
-  // this[args[0]] = {}; 
-  // this[args[0]][args[1]] = {};
-  // this[args[0]][args[1]][args[2]] = {};
+  this[args[0]] = {}; 
+  this[args[0]][args[1]] = {};
+  this[args[0]][args[1]][args[2]] = {};
   
-  that = this; 
+  // that = this; 
   
-  current = args[0];
-  this[current] = {};
+  // current = args[0];
+  // this[current] = {};
   
   
-  for(var i = 0; i<args.length; i++) {
-    current = that[current];
-    current[args[i]] = {};
-  };
-  
-
+  // for(var i = 0; i<args.length; i++) {
+  //   current = that[current];
+  //   current[args[i]] = {};
+  // };
   
 };
 
@@ -24,17 +22,17 @@ test( "hello test", function() {
   ok( 1 == "1", "Passed!" );
 });
 
-test("call namespace function", function() {
+test("call namespace function with 1 level", function() {
   namespace('app');
   ok(app.View = function(){}, "Passed!");
 });
 
-test("call namespace function", function() {
+test("call namespace function with 2 levels", function() {
   namespace('app.models');
   ok(app.models.View = function(){}, "Passed!");
 });
 
-test("call namespace function", function() {
+test("call namespace function with 3 levels", function() {
   namespace('app.models.test');
   ok(app.models.test.View = function(){}, "Passed!");
 });
