e1aed77c983c88c881a0f33ef13b5f55de2bca89
Pair <pair@basic.(none)>
2013-10-17 02:50:05 +0000

    first try at mashup

---
+++ tmp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tmp::Application.config.secret_key_base = '20153f8d79b6aac2003650fc8ed5aa60dc222b40e980688504e752682329ef3b6c5ecf6d189c3e225c09fd09ea1a0ae83c814271f9de82156c6f57233b5d40e7'
